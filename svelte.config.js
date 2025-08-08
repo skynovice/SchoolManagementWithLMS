4e24a290495e161cec6df582966f44125ba24730
5 Star Go Green <info@5stargogreen.com>
2025-08-08 12:11:56 +0700

    Fix Node.js version for Vercel deployment - Add vercel.json and update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,9 @@ import adapter from '@sveltejs/adapter-auto';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			runtime: 'nodejs20.x'
+		}),
 		alias: {
 			'$stores': 'src/stores',
 			'$components': 'src/components'
